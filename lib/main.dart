1fb2c10eccfebd0e0996ff7d03983f78ee21b824
Simone Piatti <piattsi@usi.ch>
2022-02-28 17:42:47 +0100

    Base structures

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   runApp(const MyApp());
 }
 
@@ -11,20 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'cross array task',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        // This is the theme of the application.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Home Page'),
     );
   }
 }
@@ -32,14 +27,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  // This widget is the home page of the application.
 
   final String title;
 
@@ -48,68 +36,213 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    SchoolForm(),
+    Text(
+      'Index 1: Programmazione a blocchi',
+      style: optionStyle,
+    ), //BlockBasedImplementation()
+    Text(
+      'Index 2: disegno',
+      style: optionStyle,
+    ), //GestureImplementation()
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            _widgetOptions.elementAt(_selectedIndex),
+          ],
+        ),
+      ),
+      // MENU ON THE LEFT
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Menu'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ListTile(
+              leading: const Icon(
+                Icons.home,
+                color: Colors.black,
+                size: 24.0,
+              ),
+              title: const Text('Casa'),
+              onTap: () {
+                setState(() {
+                  _selectedIndex = 0;
+                  Navigator.pop(context);
+                });
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.schema,
+                color: Colors.black,
+                size: 24.0,
+              ),
+              title: const Text('Programmazione a blocchi'),
+              onTap: () {
+                setState(() {
+                  _selectedIndex = 1;
+                  Navigator.pop(context);
+                });
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.draw,
+                color: Colors.black,
+                size: 24.0,
+              ),
+              title: const Text('Disegno'),
+              onTap: () {
+                setState(() {
+                  _selectedIndex = 2;
+                  Navigator.pop(context);
+                });
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+/// Form for save the data of the class and the school
+class SchoolForm extends StatefulWidget {
+  const SchoolForm({Key? key}) : super(key: key);
+
+  @override
+  SchoolFormState createState() {
+    return SchoolFormState();
+  }
+}
+
+/// This holds data related to the form for the class and the school.
+class SchoolFormState extends State<SchoolForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              //School name field
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Nome della scuola',
+              ),
+              // The validator receives the text that the user has entered.
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Campo obbligatorio';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              //Number of the class
+              //Class number field
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Classe',
+              ),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ], // Only numbers can be entered
+              // The validator receives the text that the user has entered.
+            ),
+            TextFormField(
+              //Section of the class
+              //Class number field
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Sezione',
+              ),
+              // The validator receives the text that the user has entered.
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Submitted corrected')),
+                  );
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/// Implementation for the gestures-based GUI
+class GestureImplementation extends StatefulWidget {
+  const GestureImplementation({Key? key}) : super(key: key);
+
+  @override
+  GestureImplementationState createState() {
+    return GestureImplementationState();
+  }
+}
+
+/// State for the gesture-based GUI
+class GestureImplementationState extends State<GestureImplementation> {
+  @override
+  Widget build(context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+/// Implementation for the block-based programming GUI
+class BlockBasedImplementation extends StatefulWidget {
+  const BlockBasedImplementation({Key? key}) : super(key: key);
+
+  @override
+  BlockBasedImplementationState createState() {
+    return BlockBasedImplementationState();
+  }
+}
+
+/// State for the block-based programming GUI
+class BlockBasedImplementationState extends State<BlockBasedImplementation> {
+  @override
+  Widget build(context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
